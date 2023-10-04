4865d6f65744633d89f8f757b0ffc7e044dafa76
Debbie Davila <macbookair@Debbie-Davilas-MacBook-Air.local>
2023-10-04 16:24:34 -0400

    created some javascript functions

---
+++ index.js
@@ -0,0 +1,20 @@
+function hello() {      // build or define a function
+    console.log("hello"); 
+
+}
+hello()         // call function
+const firstName = "Todd"
+console.log(
+
+)
+
+
+function add(num1, num2){
+    console.log(num1 + num2)
+}
+add(10, 20)
+function multiply(num1, num2,num3){
+    console.log(num1 * num2 * num3)
+
+}
+multiply(10,20,30)
